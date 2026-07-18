8ae79fa30eb6cb51c30fcbafd053722789fc3509
mogonaba <mogonaba@example.com>
2026-07-18 16:51:16 +0000

    Fix: creer le dossier data automatiquement

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 const bcrypt = require('bcryptjs');
 const { randomUUID } = require('crypto');
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
 
-const db = new Database(path.join(__dirname, 'data', 'cereales-flex.db'));
+const db = new Database(path.join(dataDir, 'cereales-flex.db'));
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
